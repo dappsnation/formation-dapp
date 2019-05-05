b63ee1ad2c6f46979bf98a996073923b11cc40c9
pldespaigne <pl.despaigne@gmail.com>
2019-05-05 19:52:40 +0200

    add contract

--- contracts/greeter.sol
+++ contracts/greeter.sol
@@ -1,3 +1,17 @@
 pragma solidity ^0.5.1;
 
-contract Greeter {}
+contract Greeter {
+
+    string public message;
+
+    event messageSet(string newMessage);
+
+    constructor(string memory initMessage) public {
+        message = initMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+        emit messageSet(newMessage);
+    }
+}
