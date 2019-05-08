b3a56c80313daff0b19c29212aab74f52538faf3
pldespaigne <pl.despaigne@gmail.com>
2019-05-08 11:35:56 +0200

    add wallet

--- main.js
+++ main.js
@@ -9,35 +9,84 @@ let GREETER_WAFFLE;
 let greeter;
 let disconnected;
 let connected;
-
+let balanceElem;
+let createElem;
+let importWalletElem;
+let DEFAULT_NETWORK = 'goerli';
 
 window.onload = async () => {
     messageElem = document.getElementById('message');
     addressElem = document.getElementById('address');
+    balanceElem = document.getElementById('balance');
     metamaskElem = document.getElementById('metamask');
+    createElem = document.getElementById('create');
+    importWalletElem = document.getElementById('import-wallet');
     newMessageElem = document.getElementById('new-message');
     sendElem = document.getElementById('send');
     disconnectedElem = document.getElementById('disconnected');
     connectedElem = document.getElementById('connected');
+    importContainerElem = document.getElementById('import-container');
+    mnemonicContainerElem = document.getElementById('mnemonic-container');
+    mnemonicElem = document.getElementById('mnemonic');
+    importButtonElem = document.getElementById('import-button');
+    importElem = document.getElementById('mnenomic-import');
 
     await fetch('build/Greeter.json')
         .then(res => res.json())
         .then(res => GREETER_WAFFLE = res);
 
     metamaskElem.addEventListener('click', async () => {
-        [addressElem.innerHTML] = await ethereum.enable();
+        const [address] = await ethereum.enable();
         provider = new ethers.providers.Web3Provider(ethereum);
-        provider = provider.getSigner();
-        greeter = new ethers.Contract(GREETER_ADDRESS, GREETER_WAFFLE.abi, provider);
-        messageElem.innerHTML = await greeter.message();
-        greeter.on('messageSet', newMessage => messageElem.innerHTML = newMessage);
+        let signer = provider.getSigner();
+
+        init(address, signer);
+    });
+
+    createElem.addEventListener('click', () => {
+        provider = ethers.getDefaultProvider(DEFAULT_NETWORK);
+        let wallet = ethers.Wallet.createRandom();
+        wallet = wallet.connect(provider);
+        mnemonicElem.innerHTML = wallet.mnemonic;
+        mnemonicContainerElem.hidden = false;
+
+        init(wallet.address, wallet);
+    });
+
+    importWalletElem.addEventListener('click', () => {
         disconnectedElem.hidden = true;
-        connectedElem.hidden = false;
+        importContainerElem.hidden = false;
+    });
+
+    importButtonElem.addEventListener('click', () => {
+        provider = ethers.getDefaultProvider(DEFAULT_NETWORK);
+        const mnemonic = importElem.value;
+        importElem.value = '';
+        let wallet = ethers.Wallet.fromMnemonic(mnemonic);
+        wallet = wallet.connect(provider);
+        mnemonicElem.innerHTML = wallet.mnemonic;
+        mnemonicContainerElem.hidden = false;
+        importContainerElem.hidden = true;
+
+        init(wallet.address, wallet);
     });
 
     sendElem.addEventListener('click', async () => {
         await greeter.setMessage(newMessageElem.value);
         newMessageElem.value = '';
     });
+
+    async function init(address, providerOrSigner) {
+        addressElem.innerHTML = address;
+        greeter = new ethers.Contract(GREETER_ADDRESS, GREETER_WAFFLE.abi, providerOrSigner);
+        messageElem.innerHTML = await greeter.message();
+        greeter.on('messageSet', newMessage => messageElem.innerHTML = newMessage);
+        disconnectedElem.hidden = true;
+        connectedElem.hidden = false;
+        let provider = providerOrSigner;
+        if (!!providerOrSigner.provider) provider = providerOrSigner.provider;
+        balanceElem.innerHTML = ethers.utils.formatEther(await provider.getBalance(address));
+        provider.on(address, balance => balanceElem.innerHTML = ethers.utils.formatEther(balance));
+    }
 }
 
